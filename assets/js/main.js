2707f870502c6cdb0dff3374366b2ede67f4f163
Ferry9 <ferry9@gmail.com>
2023-11-02 23:34:29 +0100

    commit 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
+// Fonction qui est appelée lorsque le bouton de recherche de météo est cliqué
 function buttonClickGET() {
-    const ville = document.getElementById("text").value; // Récupère la valeur de l'input avec l'ID "text"
+    // Récupère la valeur de l'input avec l'ID "text" (nom de la ville)
+    const ville = document.getElementById("text").value;
+    
+    // Construit l'URL de l'API en utilisant la valeur de l'input
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric";
     
+
+    
+    // Utilise jQuery pour envoyer une requête GET à l'URL de l'API
     $.get(url, callBackGetSuccess).done(function() {
         //alert( "second success" );
     })
@@ -15,8 +22,12 @@ function buttonClickGET() {
     });
 }
 
+// Fonction de rappel qui est appelée lorsque la requête GET à l'API réussit
 function callBackGetSuccess(data) {
+    // Récupère l'élément HTML avec l'ID "zone_meteo"
     const element = document.getElementById("zone_meteo");
+    
+    // Remplit l'élément HTML avec les informations météorologiques récupérées de l'API
     element.innerHTML = 
     "La ville est : " + data.name + "<br>" +
     "Le code pays est : " + data.sys.country + "<br>" +
