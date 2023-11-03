5cc1c6c7b3fd78a06ed62ee5bc1dd6aa3049a1bb
Ferry9 <ferry9@gmail.com>
2023-11-03 11:26:25 +0100

    vérification des commentaire/ajout de commentaire à fin de mieux comprendre le code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,16 @@ function enterKeyUp() {
     if (event.keyCode === 13) {
         buttonClickGET();
     }
-}
+}  // Fin de la fonction qui vérifie l'évènement du clic de la touche entré de l'ordinateur...
+
+
 
 // Fonction qui est appelée lorsque le bouton de recherche de météo est cliqué
 function buttonClickGET() {
     // Récupère la valeur de l'input avec l'ID "text" (nom de la ville)
     const ville = document.getElementById("text").value;
+
+    
     
     // Construit l'URL de l'API en utilisant la valeur de l'input
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric";
@@ -26,12 +30,23 @@ function buttonClickGET() {
     .always(function() {
         //alert( "finished" );
     });
-}
+} // Fin
+
+
+
 
 // Fonction de rappel qui est appelée lorsque la requête GET à l'API réussit
 function callBackGetSuccess(data) {
     // Récupère l'élément HTML avec l'ID "zone_meteo"
     const element = document.getElementById("zone_meteo");
+    const element2 = document.getElementById("image");
+
+    // Obtenez le code de condition de la météo
+    const conditionCode = data.weather[0].id;
+
+    //Déterminer l'image appropriée à afficher
+    let imgSrc = "https://openweathermap.org/img/wn/" + conditionCode + "@2x.png";
+
     
     // Remplit l'élément HTML avec les informations météorologiques récupérées de l'API
     element.innerHTML = 
@@ -42,7 +57,16 @@ function callBackGetSuccess(data) {
     "Le taux d'humidité est de : " + data.main.humidity + "%" + "<br>" +
     "La pression est de : " + data.main.pressure + "hPa" + "<br>" +
     "Le vent souffle à : " + data.wind.speed + "m/s";
-}
+    // Fin du remplissage des informations envoyer par l'API...
+
+    
+    // Ajoute la balise <img> à la fin du contenu HTML
+    element2.insertAdjacentHTML('beforeend', "<img src='" + imgSrc + "' alt='" + data.weather[0].description + "'>");
+    // Fin de l'ajout d'images météo synchronisée fonction du temps qu'il fait...
+}  // Fin de la fonction de rappel...
+
+
+
 
 // Fonction principale qui est exécutée lors du chargement de la page
 function main() {
