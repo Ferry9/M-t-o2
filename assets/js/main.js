791a122507969aef5d62399eca33ee727df4a285
Ferry9 <ferry9@gmail.com>
2023-11-07 12:14:29 +0100

    A la demande du coach.  Faire les commantaires

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,61 @@
-// J'ajoute un événement pour écouter les frappes sur l'élément avec l'id de "text"
-document.getElementById("text").addEventListener("keyup", function(event) {
-    // Je vérifie si la touche pressée est la touche "Entrée" (keyCode 13)
-    if (event.keyCode === 13) {
-       // J'empêche le comportement par défaut de la touche "Entrée" (qui est de soumettre un formulaire)
-       event.preventDefault();
-       // Je déclenche un clic sur l'élément avec  l'id du "container"
-       container.click();
-    }
+// Fonction pour écouter l'évènement au click sur la touche numéro 13 (entré)...
+document.getElementById("text").addEventListener("keyup", function (event) {
+  // Condition pour vérifie si la touche pressée est la touche "Entrée" (keyCode 13)
+  if (event.keyCode === 13) {
+    // Emmpêche le comportement par défaut de la touche "Entrée" ('soumission du formulaire)
+    event.preventDefault();
+    // Déclanchement au clic sur le boutton avec l'id du "container"
+    container.click();
+  }
 });
 
-// J'ajoute un événement pour écouter les clics sur l'élément avec l'ID "container"
-container.addEventListener('click', () => {
-    // Je récupère la valeur saisie dans l'élément avec l'ID "text"
-    const valeur_saisie = document.getElementById("text").value;
-    // Je récupère l'élément avec l'ID "zone_meteo"
-    const element = document.getElementById("zone_meteo");
-    // Je fais une requête fetch vers l'API OpenWeatherMap avec la valeur saisie
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + valeur_saisie + "&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric")
-    .then(response => response.json()) 
-    .then(data => {
-         // Je vérifie si le code de retour (cod) est "404" (indiquant une erreur)
-         if (data.cod === "404") {
-             // Si c'est le cas, j'affiche un message d'erreur dans l'élément "zone_meteo"
-             element.innerHTML = "Erreur: La région saisie n'a pas été trouvée.";
-         } else {
-             // Sinon, j'affiche les informations météorologiques dans l'élément "zone_meteo"
-             element.innerHTML = 
-             "La ville est : " + data.name + "<br>" +
-             "Le code pays est : " + data.sys.country + "<br>" +
-             "La temperature est de : " + data.main.temp + "°C" + "<br>" +
-             "Le temps est : " + data.weather[0].description + "<br>" +
-             "Le taux d'humidité est de : " + data.main.humidity + "%" + "<br>" +
-             "La pression est de : " + data.main.pressure + "hPa" + "<br>" +
-             "Le vent souffle à : " + data.wind.speed + "m/s";
-         }
-         // Fin du remplissage des informations envoyées par l'API...
+// Fonction pour écouter l'évènement au click du boutton
+container.addEventListener("click", () => {
+  // Récupération de la valeur saisie dans l'input de type texte(id: text)...
+  const valeur_saisie = document.getElementById("text").value;
+  // Récupération le l'id ou seront affichées les résultas (zone météo)...
+  const element = document.getElementById("zone_meteo");
+
+  // Requêtte vers l'api avec la valeur saisie dans l'id text...
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      valeur_saisie +
+      "&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Vérification si le code de retour est 404. Si c'est le cas, affichage erreur ...
+      if (data.cod === "404") {
+        // Si c'est le code 404, écrire à l'utilisateur le message d'erreur...
+        element.innerHTML = "Erreur: La région saisie n'a pas été trouvée.";
+      } else {
+        // Dans le cas contrire, afficher les information reçu à l'utilisateur...
+        element.innerHTML =
+          "La ville est : " +
+          data.name +
+          "<br>" +
+          "Le code pays est : " +
+          data.sys.country +
+          "<br>" +
+          "La temperature est de : " +
+          data.main.temp +
+          "°C" +
+          "<br>" +
+          "Le temps est : " +
+          data.weather[0].description +
+          "<br>" +
+          "Le taux d'humidité est de : " +
+          data.main.humidity +
+          "%" +
+          "<br>" +
+          "La pression est de : " +
+          data.main.pressure +
+          "hPa" +
+          "<br>" +
+          "Le vent souffle à : " +
+          data.wind.speed +
+          "m/s";
+      }
+      // Fin du remplissage des informations envoyées par l'API...
     });
 });
